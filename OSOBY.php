3e709c6ae1a702504dc43eaea63346156cfb5b68
MBurdajewicz <90000936+MBurdajewicz@users.noreply.github.com>
2021-10-10 18:37:21 +0200

    Add files via upload

--- OSOBY.php
+++ OSOBY.php
@@ -1,53 +1,45 @@
 <!DOCTYPE html>
-
 <html>
-<body>
-    
-          
+<body>         
     <?php
         if(!isset($_POST['person'])  && !isset($_POST['buttonAvg'])) {
             echo  <<< zmienna
             <h3> Ilość osób w rodzinie </h3>
-            <form method="post">
+            <form method="POST">
             <input type="number" name="person" placeholder="Podaj ilość osób"> <br> <br>
-            <input type="submit" value="OK">
+            <input type="submit" value="OK"> <br><br><br>           
             </form>
             zmienna;
         }
     ?>   
-    <?php   
-        
+    <?php          
         if(!empty($_POST['person']))
         {
             echo "<h3> Ilość osób w rodzinie: $_POST[person] </h3>";
-            //echo $_POST['person']; 
             echo <<<wiek
-                <form method="POST">
+                <form method="POST">          
             wiek;
-                for ($i=1; $i <= $_POST['person']; $i++ ) {
+                for ($i=1; $i <= $_POST['person']; $i++) {
                 echo "<input type=\"number\" name=\"age$i\" placeholder=\"Podaj wiek osoby\" > <br>";
                 }
                 echo '<input type="submit" name="buttonAvg" value="oblicz wiek">';
             echo <<<wiek
-                 
-                </form method="POST">
+                </form>        
             wiek;
-        }
+        }   
         if(isset($_POST['buttonAvg']))
         {
-            $ageAvg= 0
-            foreach ($_POST as $key => $value){
-                echo "$key: $value<br>"
-                if{
-                    ($key != 'buttonAvg'){
-                        $ageAvg=$ageAvg+$value;
-                    }
-                }
-                echo "Średni wiek: $ageAvg";
-
+            $ageAvg= 0;
+            foreach ($_POST as $key => $value) {       
+                if ($key != 'buttonAvg'){
+                    echo "$key: $value <br>";
+                    $ageAvg=$ageAvg+$value;                       
+                }           
             }
-            //print_r($_POST);
-            //echo "Średni wiek: ";
+            echo number_format($ageAvg, 2). "<br> <br> <br>" ; //zaokrąglenie do 2 liczb po przecinku 
+            echo <<<back
+            <input type="submit" value="Wróć" onClick="location='OSOBY.php'"/> 
+            back; // powrót na początek
         }
     ?>
 </body>
